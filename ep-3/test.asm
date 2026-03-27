bcf5b99c9d64d5ce75731d1814199262255d74bc
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2026-03-27 23:54:07 +0500

    wrote a simple assembly program

--- ep-3/test.asm
+++ ep-3/test.asm
@@ -1,2 +1,20 @@
-    lda #$42
-    sta $2000
+    .org $8000
+reset:
+    lda #$aa
+    sta $6000
+
+    lda #$55
+    sta $4000
+
+loop:
+    lda $6000
+    sta $4000
+
+    lda $4000
+    sta $6000
+
+    jmp loop
+
+    .org $fffc
+    .word reset
+    .word $0000
