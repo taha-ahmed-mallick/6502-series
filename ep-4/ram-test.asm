770192e3e7c1a1267b55e7bb3775c62b7d540ddb
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2026-04-19 11:39:13 +0500

    wrote the code to test the ram with updated bus timing

---
+++ ep-4/ram-test.asm
@@ -0,0 +1,31 @@
+    .org $8000
+reset:
+    sta $6000
+    stx $6000
+    sty $6000
+    ; all registers weren't set to 0 (they were in a random state)
+    lda #$aa
+    sta $6000
+
+    ldx #$55
+    stx $4000
+
+    ; Stack check
+    pha
+    phx
+    plx
+    pla
+
+    lda #$00
+    ldx #$00
+loop:
+    lda $6000
+    ldx $4000
+
+    sta $4000
+    stx $6000
+    jmp loop
+
+    .org $fffc
+    .word reset
+    .word $0000
