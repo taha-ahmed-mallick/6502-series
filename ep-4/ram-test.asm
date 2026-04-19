5df87dedccf74f6679cda7576604eca9f538f96e
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2026-04-19 16:17:00 +0500

    passed the ram test, now it retains the last bit in the memory

--- ep-4/ram-test.asm
+++ ep-4/ram-test.asm
@@ -6,13 +6,40 @@ reset:
     ; all registers weren't set to 0 (they were in a random state)
     lda #$aa
     sta $6000
+    lda $6000
 
     ldx #$55
     stx $4000
+    lda $4000
+
+    lda #$55
+    sta $4000
+    lda $4000
+
+    ldy #$ff
+    sty $2000
+    lda $2000
+
+    lda #$00
+    sta $3000
+    lda $3000
+
+    lda #$0f
+    sta $5000
+    lda $5000
+
+    lda #$f0
+    sta $7000
+    lda $7000
 
     ; Stack check
+    ; A = $f0
+    ; X = $55
+    ; Y = $ff
     pha
     phx
+    phy
+    ply
     plx
     pla
 
